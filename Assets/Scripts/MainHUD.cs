943fe146e50bb9531554dcd37811220eaa082764
P-Allgeier <moonofcrows@icloud.com>
2026-08-24 15:16:18 +0200

    Add ShowDialogue class and function

---
+++ Assets/Scripts/MainHUD.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class MainHUD : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+}
--- Assets/Scripts/ShowDialogue.cs
+++ Assets/Scripts/ShowDialogue.cs
@@ -1,15 +1,38 @@
+using System.Collections.Generic;
+using System.Linq;
+using TMPro;
+using Unity.Mathematics;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ShowDialogue : MonoBehaviour
 {
 
+    public GameObject diaBox;
+    
+    private TextMeshProUGUI textField;
+
+    public void Start()
+    {
+        getTextField();
+    }
+
     public void showDialogue(string dialogue)
     {
-        
+        if(this.textField != null)
+        {
+            this.textField.text = dialogue;
+        }
     }
 
     public void getTextField()
     {
+        this.textField = this.GetComponentInParent<TextMeshProUGUI>();
 
+        if (this.textField != null) 
+        {
+            Debug.Log("Got the text thingy.");
+            showDialogue("When the Bingus is Schmungus.");
+        }
     }
 }
