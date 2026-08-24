35fdbdb1c913f76d3da5b174250fe56041d2e566
P-Allgeier <moonofcrows@icloud.com>
2026-08-24 14:37:32 +0200

    a

---
+++ Assets/Scripts/ShowDialogue.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class ShowDialogue : MonoBehaviour
+{
+
+    public void showDialogue(string dialogue)
+    {
+        
+    }
+
+    public void getTextField()
+    {
+
+    }
+}
