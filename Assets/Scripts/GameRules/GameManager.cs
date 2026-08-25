515f0d64153f2f4ea2f4d0604d658c83da021fd3
rinorbqr <rinorbqr@gmail.com>
2026-08-25 21:32:43 +0200

    create random PlayerPaper object logic and call function on start

--- Assets/Scripts/GameRules/GameManager.cs
+++ Assets/Scripts/GameRules/GameManager.cs
@@ -3,10 +3,12 @@ using UnityEngine;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private PaperReq req;
+    [SerializeField] private PlayerPaper playerPaper;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         this.req = new PaperReq();
+        playerPaper = new PlayerPaper();
     }
 
     // Update is called once per frame
--- Assets/Scripts/GameRules/PlayerPaper.cs
+++ Assets/Scripts/GameRules/PlayerPaper.cs
@@ -1,3 +1,5 @@
+using UnityEngine;
+
 public class PlayerPaper : Paper
 {
     public string name;
@@ -5,9 +7,52 @@ public class PlayerPaper : Paper
     public string sex;
     public float id;
     public float height;
+    public float age;
 
     public PlayerPaper()
     {
+        GenerateRandomPaper();
+    }
 
+    private void GenerateRandomPaper()
+    {
+        string[] names = RulesList.getNames();
+        name = names[UnityEngine.Random.Range(0, names.Length)];
+
+        origin = GenerateRandomOrigin();
+
+        string[] allSexes =
+        {
+            "Male", "Female", "Nonbinary", "Agender", "Fluid",
+            "Glorb", "Vorb", "Zorb", "Blorb"
+        };
+        sex = allSexes[UnityEngine.Random.Range(0, allSexes.Length)];
+
+        height = UnityEngine.Random.Range(100f, 220f);
+
+        age = UnityEngine.Random.Range(0f, 108f);
+
+        id = GenerateRandomID();
+    }
+
+    private string GenerateRandomOrigin()
+    {
+        int len = UnityEngine.Random.Range(3, 12);
+        string s = "";
+        for (int i = 0; i < len; i++)
+        {
+            s += (char)UnityEngine.Random.Range('A', 'Z' + 1);
+        }
+        return s;
+    }
+
+    private long GenerateRandomID()
+    {
+        string idStr = "";
+        for (int i = 0; i < 10; i++)
+        {
+            idStr += UnityEngine.Random.Range(0, 10).ToString();
+        }
+        return long.Parse(idStr);
     }
 }
