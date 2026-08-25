365eeab2a2ed54a3f28003b72961fe1024ba1018
P-Allgeier <moonofcrows@icloud.com>
2026-08-25 15:08:25 +0200

    implement rough age rule generation

--- Assets/Scripts/GameRules/PaperReq.cs
+++ Assets/Scripts/GameRules/PaperReq.cs
@@ -6,9 +6,28 @@ public class PaperReq : Paper
     public float id;
     public float height;
 
+    private Rule ageRule;
+    private Rule heightRule;
+    private Rule originRule;
+    private Rule nameRule;
+    private Rule sexRule;
+    private Rule idRule;
+
+
+    // randomize a requirement paper, then later, enforce the rules over player paper vs reqpaper somehow.
     public PaperReq()
     {
-           
+        makeAgeRule();
+    }
+
+    private void makeAgeRule()
+    {
+        //randomize for range of allowed ages
+        Tuple<int, int> ageRange = new Tuple<int, int>(random.Range(0, 54), random.Range(54, 108));
+        //randomize three specific ages that are not allowed
+        float[] specificExcludedAges = new float[] { random.Range(0, 108), random.Range(0, 108), random.Range(0, 108) };
+        
+        this.ageRule = new AgeRule(ageRange, specificExcludedAges);
     }
 
 }
--- Assets/Scripts/GameRules/RuleRules/AgeRule.cs
+++ Assets/Scripts/GameRules/RuleRules/AgeRule.cs
@@ -2,16 +2,29 @@ class AgeRule : Rule
 {
     // Age Rules are rules concerning the player's age, things like
     // minimum or maximum age requirements.
-
-    //TODO: come up with a list of rules this can choose from.
+    float playerAge;
 
     public AgeRule()
     {
-
+        Tuple<int, int> ageRange;
+        float[] specificExcludedAges;
     }
 
     public override void enforceRule()
     {
+        if (playerAge < ageRange.Item1 || playerAge > ageRange.Item2)
+        {
+            // Player is outside the allowed age range
+            // Handle the violation of the rule here
+        } else if (specificExcludedAges.Contains(playerAge))
+        {
+            // Player's age is specifically excluded
+            // Handle the violation of the rule here
+        }
+    }
 
+    public checkAge(float age)
+    {
+        this.playerAge = age;
     }
 }
